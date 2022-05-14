d38e955d1aebf448f154c9b7d0251f3b7d7f1ca0
Dvij1995 <105531708+Dvij1995@users.noreply.github.com>
2022-05-13 20:59:58 -0700

    Create Printnum.cpp

---
+++ Printnum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;    
+    return 0;
+}
+O/P
+Enter an integer: 23
+You entered 23
